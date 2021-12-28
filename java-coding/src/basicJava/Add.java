fbd8514011a03e21758aeb70b4d14b8142ce3715
Aditi-2840 <aditikumari2840@gmail.com>
2021-12-29 00:09:26 +0530

    commit

---
+++ java-coding/src/basicJava/Add.java
@@ -0,0 +1,11 @@
+package basicJava;
+
+import java.util.*;
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(2+3);
+	}
+
+}
